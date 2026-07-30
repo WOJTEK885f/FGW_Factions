103f24ae40088bb5ec7faf7a8e74dd712f1518f7
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-30 02:34:22 +0200

    Refactor include paths in script_component.hpp and remove unused variables in XEH_preInit.sqf

--- addons/factions/script_component.hpp
+++ addons/factions/script_component.hpp
@@ -1,9 +1,9 @@
 #define COMPONENT factions
 #define COMPONENT_BEAUTIFIED Factions
-#include "\z\MOD_PREFIX\addons\main\script_mod.hpp"
+#include "\z\gr7bow_fgw_factions\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#include "\z\MOD_PREFIX\addons\main\script_macros.hpp"
+#include "\z\gr7bow_fgw_factions\addons\main\script_macros.hpp"
