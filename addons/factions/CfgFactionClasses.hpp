e95dbd43b8fb1102dd898e88f0cc207ece663a65
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-31 01:45:24 +0200

    Add faction definitions and localization for Alpha, Atov, CFR, FCA, Uman, and VFF

--- addons/factions/CfgFactionClasses.hpp
+++ addons/factions/CfgFactionClasses.hpp
@@ -1,1 +1,9 @@
-#include "Pozna\Faction.hpp"
+class CfgFactionClasses {
+    #include "Alpha\Faction.hpp"
+    #include "Atov\Faction.hpp"
+    #include "CFR\Faction.hpp"
+    #include "FCA\Faction.hpp"
+    #include "Pozna\Faction.hpp"
+    #include "Uman\Faction.hpp"
+    #include "VFF\Faction.hpp"
+};
---
+++ addons/factions/FCA/Faction.hpp
@@ -0,0 +1,7 @@
+class GVAR(FCA) {
+    displayName = "$STR_gr7bow_fgw_factions_factions_FCA_FactionName";
+    priority = 1;
+    side = 2; // INDFOR
+    icon = "\a3\Data_f\cfgFactionClasses_IND_ca.paa";
+    flag = "..\data\flags\flag_Free_Cossacks_Army_co.paa";
+};
