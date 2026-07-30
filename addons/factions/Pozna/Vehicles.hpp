17c160788f3be9c3d66c12b44f40dbb1ce04034c
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-31 00:11:23 +0200

    Refactor FGW_B_Pozna_Fighter class: streamline magazine definitions and remove redundant items

--- addons/factions/Pozna/Vehicles.hpp
+++ addons/factions/Pozna/Vehicles.hpp
@@ -1,8 +1,8 @@
 class FGW_B_Pozna_Fighter : B_Soldier_F {
-        author = AUTHOR;
         _generalMacro = "FGW_B_Pozna_Fighter";
         scope = 2;
         scopeCurator = 2;
+        editorPreview = "";
 
         displayName = "$STR_gr7bow_fgw_factions_factions_Pozna_Fighter";
         faction = QGVAR(Pozna);
@@ -14,49 +14,18 @@ class FGW_B_Pozna_Fighter : B_Soldier_F {
         weapons[] = {"CUP_arifle_SAIGA_MK03", "Throw", "Put"};
         respawnWeapons[] = {"CUP_arifle_SAIGA_MK03", "Throw", "Put"};
 
-        // Amunicja i granaty
         magazines[] = {
             "CUP_HandGrenade_RGD5",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M"
+            MAG_15("CUP_10Rnd_762x39_SaigaMk03_M")
         };
         respawnMagazines[] = {
             "CUP_HandGrenade_RGD5",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M",
-            "CUP_10Rnd_762x39_SaigaMk03_M"
+            MAG_15("CUP_10Rnd_762x39_SaigaMk03_M")
         };
 
-        // Medykamenty i przedmioty z ACE
-        Items[] = {"FirstAidKit", "ace_earplugs"};
-        RespawnItems[] = {"FirstAidKit", "ace_earplugs"};
+        Items[] = {"FirstAidKit"};
+        RespawnItems[] = {"FirstAidKit"};
 
-        // Kamizelka, okulary/kominiarka, oraz przedmioty z ekwipunku (mapa, kompas itp.)
         linkedItems[] = {
             "SP_Tactical1_Tan",
             "CUP_RUS_Balaclava_blk",
--- addons/factions/script_component.hpp
+++ addons/factions/script_component.hpp
@@ -8,6 +8,7 @@
 
 #include "\z\gr7bow_fgw_factions\addons\main\script_macros.hpp"
 
+#define MAG_2(a) a, a
 #define MAG_3(a) a, a, a
 #define MAG_4(a) a, a, a, a
 #define MAG_5(a) a, a, a, a, a
@@ -16,7 +17,11 @@
 #define MAG_8(a) a, a, a, a, a, a, a, a
 #define MAG_9(a) a, a, a, a, a, a, a, a, a
 #define MAG_10(a) a, a, a, a, a, a, a, a, a, a
-#define MAG_12(a) a, a, a, a, a, a, a, a, a, a, a, a
+#define MAG_11(a) a, a, a, a, a, a, a, a, a, a, a
+#define MAG_12(a) a, a, a, a, a, a, a, a, a, a,	a, a
+#define MAG_13(a) a, a, a, a, a, a, a, a, a, a,	a, a, a
+#define MAG_14(a) a, a, a, a, a, a, a, a, a, a,	a, a, a, a
+#define MAG_15(a) a, a, a, a, a, a, a, a, a, a,	a, a, a, a, a
 
 #define MAG_XX(a,b) class _xx_##a {magazine = a; count = b;}
 #define WEAP_XX(a,b) class _xx_##a {weapon = a; count = b;}
