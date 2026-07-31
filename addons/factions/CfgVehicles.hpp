2749347bf1727523f20948390428e9132d8d3818
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-31 15:24:03 +0200

    Comment out forward declarations for unused I_Soldier_F and O_Soldier_F in CfgVehicles

--- addons/factions/CfgVehicles.hpp
+++ addons/factions/CfgVehicles.hpp
@@ -1,7 +1,7 @@
 class CfgVehicles {
     // Forward declarations
-    class I_Soldier_F;
-    class O_Soldier_F;
+    //class I_Soldier_F;
+    //class O_Soldier_F;
     class B_Soldier_F;
 
     #include "Pozna\Vehicles.hpp"
