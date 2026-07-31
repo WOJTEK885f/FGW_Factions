044fb7535f439752e2604b365d0a6d821a99c73c
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-31 18:07:00 +0200

    Update requiredAddons and units in CfgPatches for Pozna faction

--- addons/factions/config.cpp
+++ addons/factions/config.cpp
@@ -10,13 +10,18 @@ class CfgPatches {
         requiredAddons[] = {
             "gr7bow_fgw_factions_main",
             "A3_Characters_F",
-            "CUP_Weapons_LoadOrder",
-            "CUP_Creatures_Military_Russia",
-            "usp_gear_body",
-            "CFP_vests"
+            "CUP_Weapons_WeaponsCore",
+            "CUP_Weapons_NVG",
+            "CUP_Creatures_Military_CDF", // Sniper Elite: Vest
+            "CUP_Creatures_Military_USMC", // Commando: Vest
+            "USP_Gear_Body", // All units: Uniforms
+            "cfp_vests" // Fighter: Vest
+            "cfp_headgear" // Commando: Helmet
         };
         units[] = {
-            "FGW_B_Pozna_Fighter"
+            "FGW_B_Pozna_Fighter",
+            "FGW_B_Pozna_Commando",
+            "FGW_B_Pozna_SniperElite"
         };
         weapons[] = {};
         VERSION_CONFIG;
