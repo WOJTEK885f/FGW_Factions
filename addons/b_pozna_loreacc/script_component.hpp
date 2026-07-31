8d013133834790cb76719f71d6f4a24588681312
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-31 18:31:14 +0200

    Move macro definitions for MAG_2 to MAG_15 from pozna addon to main for sharing

--- addons/b_pozna_loreacc/script_component.hpp
+++ addons/b_pozna_loreacc/script_component.hpp
@@ -7,18 +7,3 @@
 // #define ENABLE_PERFORMANCE_COUNTERS
 
 #include "\z\gr7bow_fgw_factions\addons\main\script_macros.hpp"
-
-#define MAG_2(a) a, a
-#define MAG_3(a) a, a, a
-#define MAG_4(a) a, a, a, a
-#define MAG_5(a) a, a, a, a, a
-#define MAG_6(a) a, a, a, a, a, a
-#define MAG_7(a) a, a, a, a, a, a, a
-#define MAG_8(a) a, a, a, a, a, a, a, a
-#define MAG_9(a) a, a, a, a, a, a, a, a, a
-#define MAG_10(a) a, a, a, a, a, a, a, a, a, a
-#define MAG_11(a) a, a, a, a, a, a, a, a, a, a, a
-#define MAG_12(a) a, a, a, a, a, a, a, a, a, a, a, a
-#define MAG_13(a) a, a, a, a, a, a, a, a, a, a, a, a, a
-#define MAG_14(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a
-#define MAG_15(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
