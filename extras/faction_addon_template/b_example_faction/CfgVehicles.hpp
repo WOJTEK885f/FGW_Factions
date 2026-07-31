d4640d4d253d9990bdaa980f5299de310d577eb9
WOJTEK885f <WOJTEK885@protonmail.com>
2026-07-31 19:41:22 +0200

    Add example faction addon template

---
+++ extras/faction_addon_template/b_example_faction/CfgVehicles.hpp
@@ -0,0 +1,51 @@
+class CfgVehicles {
+    class B_Soldier_F; // Forward declaration
+
+    class FGW_B_Example_Base: B_Soldier_F {
+        author = AUTHOR;
+        scope = 0;         // Hidden in Editor
+        scopeCurator = 0;  // Hidden in Zeus
+
+        // Common assignments
+        faction = QGVAR(Example);
+        editorSubcategory = "EdSubcat_Personnel";
+
+        // Faction identity: Example appearance and language
+        identityTypes[] = {"LanguageRUS", "Head_Russian", "Head_Euro", "Head_Enoch"};
+        genericNames = "RussianMen";
+    };
+
+    class FGW_B_Example_Unit: FGW_B_Example_Base {
+        _generalMacro = "FGW_B_Example_Unit";
+        scope = 2;
+        scopeCurator = 2;
+        editorPreview = "";
+
+        displayName = "$STR_gr7bow_fgw_factions_b_example_faction_Unit";
+
+        uniformClass = "";
+        backpack = "";
+
+        weapons[] = {"", "Throw", "Put"};
+        respawnWeapons[] = {"", "Throw", "Put"};
+
+        magazines[] = {
+        };
+        respawnMagazines[] = {
+        };
+
+        Items[] = {"FirstAidKit"};
+        RespawnItems[] = {"FirstAidKit"};
+
+        linkedItems[] = {
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch"
+        };
+        respawnLinkedItems[] = {
+            "ItemMap",
+            "ItemCompass",
+            "ItemWatch"
+        };
+    };
+};
---
+++ extras/faction_addon_template/b_example_faction/config.cpp
@@ -0,0 +1,23 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        author = AUTHOR;
+        authors[] = {"WOJTEK885"};
+        url = ECSTRING(main,url);
+        name = QUOTE(COMPONENT);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "gr7bow_fgw_factions_main",
+            "A3_Characters_F"
+        };
+        units[] = {
+            "FGW_B_Example_Unit"
+        };
+        weapons[] = {};
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgFactionClasses.hpp"
+#include "CfgVehicles.hpp"
