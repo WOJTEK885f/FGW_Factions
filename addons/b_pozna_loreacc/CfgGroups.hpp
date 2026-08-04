cbcab1d3f1d17d22b7af10bd12b1bf90a21a1947
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-05 01:02:41 +0200

    Update Infantry group name and adjust unit positions for Pozna faction

--- addons/b_pozna_loreacc/CfgGroups.hpp
+++ addons/b_pozna_loreacc/CfgGroups.hpp
@@ -4,7 +4,7 @@ class CfgGroups {
             name = "$STR_gr7bow_fgw_factions_b_pozna_loreacc_FactionName";
 
             class Infantry {
-                name = "$STR_gr7bow_fgw_factions_b_pozna_loreacc_GroupCategory_Infantry";
+                name = "$STR_A3_CfgGroups_West_BLU_F_Infantry0";
 
                 class GVAR(Group_RifleSquad) {
                     name = "$STR_gr7bow_fgw_factions_b_pozna_loreacc_Group_RifleSquad";
@@ -12,12 +12,12 @@ class CfgGroups {
                     faction = QGVAR(Pozna);
 
                     class Unit0 { side = 1; vehicle = "FGW_B_Pozna_Commando"; rank = "SERGEANT"; position[] = {0,0,0}; };
-                    class Unit1 { side = 1; vehicle = "FGW_B_Pozna_Commando"; rank = "CORPORAL"; position[] = {3,-3,0}; };
-                    class Unit2 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {3,3,0}; };
-                    class Unit3 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {6,-3,0}; };
-                    class Unit4 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {6,3,0}; };
-                    class Unit5 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {9,-3,0}; };
-                    class Unit6 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {9,3,0}; };
+                    class Unit1 { side = 1; vehicle = "FGW_B_Pozna_Commando"; rank = "CORPORAL"; position[] = {5,-5,0}; };
+                    class Unit2 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {-5,-5,0}; };
+                    class Unit3 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {10,-10,0}; };
+                    class Unit4 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {-10,10,0}; };
+                    class Unit5 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {15,-15,0}; };
+                    class Unit6 { side = 1; vehicle = "FGW_B_Pozna_Fighter";  rank = "PRIVATE";  position[] = {-15,-15,0}; };
                 };
 
                 class GVAR(Group_MarksmanTeam) {
@@ -26,8 +26,8 @@ class CfgGroups {
                     faction = QGVAR(Pozna);
 
                     class Unit0 { side = 1; vehicle = "FGW_B_Pozna_SniperElite"; rank = "CORPORAL"; position[] = {0,0,0}; };
-                    class Unit1 { side = 1; vehicle = "FGW_B_Pozna_SniperElite"; rank = "PRIVATE";  position[] = {3,-3,0}; };
-                    class Unit2 { side = 1; vehicle = "FGW_B_Pozna_SniperElite"; rank = "PRIVATE";  position[] = {3,3,0}; };
+                    class Unit1 { side = 1; vehicle = "FGW_B_Pozna_SniperElite"; rank = "PRIVATE";  position[] = {5,-5,0}; };
+                    class Unit2 { side = 1; vehicle = "FGW_B_Pozna_SniperElite"; rank = "PRIVATE";  position[] = {-5,-5,0}; };
                 };
             };
         };
