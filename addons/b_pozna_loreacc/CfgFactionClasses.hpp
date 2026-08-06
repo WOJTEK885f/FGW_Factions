c56b9a876f505dc5e54eff8327f90a642c254bbc
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-06 16:20:44 +0200

    Add new identities and subcategories for companions in Pozna faction

--- addons/b_pozna_loreacc/CfgFactionClasses.hpp
+++ addons/b_pozna_loreacc/CfgFactionClasses.hpp
@@ -7,3 +7,44 @@ class CfgFactionClasses {
         flag = "data\flags\flag_PoznaBattalion_co.paa";
     };
 };
+
+class CfgEditorSubcategories {
+    class GVAR(Companions) {
+        displayName = "$STR_gr7bow_fgw_factions_b_pozna_loreacc_Subcat_Companions";
+    };
+};
+
+class CfgIdentities {
+    class FGW_Identity_Pozna_Roman {
+        name = "Roman";
+        nameSound = "Roman";
+        face = "WhiteHead_31";
+        glasses = "USP_BEARD_BRN2";
+        speaker = "Male03RUS";
+        pitch = 1.0;
+    };
+    class FGW_Identity_Pozna_Yuriy {
+        name = "Yuriy";
+        nameSound = "Yuriy";
+        face = "LivonianHead_10";
+        glasses = "USP_BEARD_CH_MS_BLK2";
+        speaker = "Male02RUS";
+        pitch = 0.9;
+    };
+    class FGW_Identity_Pozna_Denis {
+        name = "Denis";
+        nameSound = "Denis";
+        face = "LivonianHead_3";
+        glasses = "None";
+        speaker = "Male01RUS";
+        pitch = 1.1;
+    };
+    class FGW_Identity_Pozna_Tatyana {
+        name = "Tatyana";
+        nameSound = "Tatyana";
+        face = "WomanHead_A3";
+        glasses = "None";
+        speaker = "CUP_D_Female01_RU";
+        pitch = 1.0;
+    };
+};
