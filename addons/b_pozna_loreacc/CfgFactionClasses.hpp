cb34a0d0f568d255dd06c09f2080b2be25659cd4
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-09 18:53:18 +0200

    Disable face classes for companions in CfgFaces for Pozna and Atov factions so they are not visible in game

--- addons/b_pozna_loreacc/CfgFactionClasses.hpp
+++ addons/b_pozna_loreacc/CfgFactionClasses.hpp
@@ -53,18 +53,22 @@ class WhiteHead_01;
 class CfgFaces {
     class Man_A3: Default {
         class FGW_Face_Roman: WhiteHead_31 {
+            disabled = 1;
             displayName = "FGW Roman";
             identityTypes[] = {"FGW_Face_Roman_Tag"};
         };
         class FGW_Face_Yuriy: LivonianHead_10 {
+            disabled = 1;
             displayName = "FGW Yuriy";
             identityTypes[] = {"FGW_Face_Yuriy_Tag"};
         };
         class FGW_Face_Denis: LivonianHead_3 {
+            disabled = 1;
             displayName = "FGW Denis";
             identityTypes[] = {"FGW_Face_Denis_Tag"};
         };
         class FGW_Face_Tatyana: WhiteHead_01 {
+            disabled = 1;
             displayName = "FGW Tatyana";
             identityTypes[] = {"FGW_Face_Tatyana_Tag"};
         };
