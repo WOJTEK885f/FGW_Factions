2faf41d3fe88b6be064b8f875fe653c0af07e9ac
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-09 18:07:42 +0200

    Enhance FGW Vydra-3M Flak Vest with detailed armor stats in HitpointsProtectionInfo

--- addons/o_atov_loreacc/CfgWeapons.hpp
+++ addons/o_atov_loreacc/CfgWeapons.hpp
@@ -54,19 +54,46 @@ class CfgWeapons {
     // Import the base uniform class from RHS to use as a parent for custom vest
     class rhs_vydra_3m;
 
+    // Custom Vydra-3M vest class with added armor
     class FGW_V_O_Flak_Vest_Vydra_3M: rhs_vydra_3m {
         author = AUTHOR;
         displayName = "$STR_gr7bow_fgw_factions_o_atov_loreacc_vydra_3m_flakvest";
         scope = 2; // Available in Arsenal
 
         class ItemInfo: VestItem {
-            uniformModel = "\rhsafrf\addons\rhs_infantry\gear\vests\rhs_vydra_3m"; // Matched with Vydra-3M (rhs_vydra_3m)
-            hiddenSelections[] = {"Camo1", "Camo2"}; // Matched with Vydra-3M (rhs_vydra_3m)
+            // Model configuration matched with Vydra-3M (rhs_vydra_3m)
+            uniformModel = "\rhsafrf\addons\rhs_infantry\gear\vests\rhs_vydra_3m";
+            hiddenSelections[] = {"Camo1", "Camo2"};
 
-            armor = 20; // Matched with 6B3 Flak Vest (CUP_V_O_SLA_Flak_Vest03)
-            passThrough = 1; // Matched with 6B3 Flak Vest (CUP_V_O_SLA_Flak_Vest03)
-            containerClass = "Supply80"; // Matched with Vydra-3M (rhs_vydra_3m)
-            mass = 55; // Matched with 6B3 Flak Vest (CUP_V_O_SLA_Flak_Vest03)
+            // Base stats matched with 6B3 Flak Vest (CUP_V_O_SLA_Flak_Vest03)
+            armor = 20;
+            passThrough = 1;
+            containerClass = "Supply80"; // Load matched with Vydra-3M (rhs_vydra_3m)
+            mass = 55;
+
+            // Armor stats matched with 6B3 Flak Vest (CUP_V_O_SLA_Flak_Vest03)
+            class HitpointsProtectionInfo {
+                class Chest {
+                    hitpointName = "HitChest";
+                    armor = 12;
+                    passThrough = 0.4;
+                };
+                class Diaphragm {
+                    hitpointName = "HitDiaphragm";
+                    armor = 12;
+                    passThrough = 0.4;
+                };
+                class Abdomen {
+                    hitpointName = "HitAbdomen";
+                    armor = 12;
+                    passThrough = 0.4;
+                };
+                class Body {
+                    hitpointName = "HitBody";
+                    armor = 0;
+                    passThrough = 0.4;
+                };
+            };
         };
     };
 };
