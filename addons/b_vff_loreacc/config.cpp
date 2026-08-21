75e7c313592ad0f3993a37149f4f0c2a9be1ba6b
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-22 01:22:10 +0200

    Remove duplicate requiredAddons entry

--- addons/b_vff_loreacc/config.cpp
+++ addons/b_vff_loreacc/config.cpp
@@ -11,18 +11,17 @@ class CfgPatches {
             "gr7bow_fgw_factions_main",
             "A3_Characters_F",
             "A3_Weapons_F",
-            "CUP_Weapons_WeaponsCore",          // HK416, M16A4, AA12, Colt1911, M9A1
-            "CUP_Creatures_Military_USMC",       // Interceptor vests, M1 helmet, ACR backpack
-            "CUP_Creatures_Military_Russia",     // Altyn helmet, K6-3 helmet
-            "CUP_Creatures_Military_PMC",        // PMC Unit 1 uniform, CIRAS vest
-            "rhs_main",                          // M590 shotgun, RHS Vydra vest
-            "USP_Gear_Body",                     // USP G3C uniform (Fighter, Marine)
-            "USP_Gear_Face",                     // USP balaclava
-            "cfp_uniforms",                      // CFP M81Tee, TanTee
-            "cfp_vests",                         // CFP Tactical vest
-            "cfp_headgear",                      // CFP PASGT helmet
-            "gr7bow_fgw_factions_b_pozna_loreacc", // FGW_V_B_Flak_Vest_Vydra_3M
-            "gr7bow_fgw_factions_main" // Female faces
+            "CUP_Weapons_WeaponsCore",            // HK416, M16A4, AA12, Colt1911, M9A1
+            "CUP_Creatures_Military_USMC",        // Interceptor vests, M1 helmet, ACR backpack
+            "CUP_Creatures_Military_Russia",      // Altyn helmet, K6-3 helmet
+            "CUP_Creatures_Military_PMC",         // PMC Unit 1 uniform, CIRAS vest
+            "rhs_main",                           // M590 shotgun, RHS Vydra vest
+            "USP_Gear_Body",                      // USP G3C uniform (Fighter, Marine)
+            "USP_Gear_Face",                      // USP balaclava
+            "cfp_uniforms",                       // CFP M81Tee, TanTee
+            "cfp_vests",                          // CFP Tactical vest
+            "cfp_headgear",                       // CFP PASGT helmet
+            "gr7bow_fgw_factions_b_pozna_loreacc" // FGW_V_B_Flak_Vest_Vydra_3M
         };
         units[] = {
             "FGW_B_VFF_CivilianFighter",
