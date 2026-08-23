11ee420c1341cb8d284b57df9c9f063c53aa5bb1
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-24 00:28:57 +0200

    Add compatibility addon for FGWF - FSOF soft dependency with initial configuration

---
+++ addons/compat_fsof/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        author = AUTHOR;
+        authors[] = {"WOJTEK885"};
+        url = ECSTRING(main,url);
+        name = QUOTE(COMPONENT);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "gr7bow_fgw_factions_main",
+            "A3_Characters_F",
+            "A3_Weapons_F",
+            "zee_fsof"
+        };
+        units[] = {};
+        weapons[] = {};
+        VERSION_CONFIG;
+
+        skipWhenMissingDependencies = 1;
+    };
+};
