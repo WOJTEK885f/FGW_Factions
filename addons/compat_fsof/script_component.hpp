7457394434eafabb26db6a4b9a814be9bb46a795
WOJTEK885f <WOJTEK885@protonmail.com>
2026-08-24 01:02:24 +0200

    Remove redundant fgwf prefix from addon name

--- addons/compat_fsof/script_component.hpp
+++ addons/compat_fsof/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT fgwf_compat_fsof
-#define COMPONENT_BEAUTIFIED FGWF - FSOF Compatibility
+#define COMPONENT compat_fsof
+#define COMPONENT_BEAUTIFIED FSOF Compatibility
 #include "\z\gr7bow_fgw_factions\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
