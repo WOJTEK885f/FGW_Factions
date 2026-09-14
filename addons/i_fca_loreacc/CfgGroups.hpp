fdc5867bb708771c2da65f4e608cf3e8a8a52a54
WOJTEK885f <WOJTEK885@protonmail.com>
2026-09-14 19:41:57 +0200

    Update infantry group configurations

--- addons/i_fca_loreacc/CfgGroups.hpp
+++ addons/i_fca_loreacc/CfgGroups.hpp
@@ -59,7 +59,6 @@ class CfgGroups {
                     class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Militia";  rank = "CORPORAL"; position[] = {5,-5,0}; };
                     class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Militia";  rank = "PRIVATE";  position[] = {-5,-5,0}; };
                     class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Militia";  rank = "PRIVATE";  position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Militia";  rank = "PRIVATE";  position[] = {-10,-10,0}; };
                 };
 
                 class GVAR(Group_Army_RifleCompany) {
@@ -72,6 +71,7 @@ class CfgGroups {
                     class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";  rank = "CORPORAL"; position[] = {5,-5,0}; };
                     class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";  rank = "PRIVATE";  position[] = {-5,-5,0}; };
                     class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Grenadier"; rank = "PRIVATE";  position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";  rank = "PRIVATE";  position[] = {-10,-10,0}; };
                 };
 
                 class GVAR(Group_Army_GrenadiersCompany) {
@@ -84,6 +84,7 @@ class CfgGroups {
                     class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";  rank = "CORPORAL"; position[] = {5,-5,0}; };
                     class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";  rank = "PRIVATE";  position[] = {-5,-5,0}; };
                     class Unit3 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"; rank = "PRIVATE";  position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";  rank = "PRIVATE";  position[] = {-10,-10,0}; };
                 };
 
                 class GVAR(Group_Army_ShockBattalion) {
@@ -107,60 +108,60 @@ class CfgGroups {
                     faction = QGVAR(FCA);
                     icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
 
-                    class Unit0 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"; rank = "SERGEANT"; position[] = {0,0,0}; };
+                    class Unit0 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";  rank = "SERGEANT"; position[] = {0,0,0}; };
                     class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";  rank = "CORPORAL"; position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"; rank = "PRIVATE";  position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";   rank = "PRIVATE";  position[] = {10,-10,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Militia";    rank = "PRIVATE";  position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"  rank = "PRIVATE";  position[] = {10,-10,0}; };
                     class Unit4 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"; rank = "PRIVATE";  position[] = {-10,-10,0}; };
                     class Unit5 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"; rank = "PRIVATE";  position[] = {15,-15,0}; };
-                    class Unit6 { side = 2; vehicle = "FGWF_I_FCA_Militia";    rank = "PRIVATE";  position[] = {-15,-15,0}; };
+                    class Unit6 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop"; rank = "PRIVATE";  position[] = {-15,-15,0}; };
                 };
             };
 
             class Infantry_CompanionLed {
                 name = "$STR_GR7BOW_FGWF_Main_Subcat_Infantry_CompanionLed";
 
-                class GVAR(Group_MilitiaSquad_Ostap) {
-                    name = CSTRING(Group_MilitiaSquad_Ostap);
-                    side = 2;
-                    faction = QGVAR(FCA);
-                    icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
-
-                    class Unit0 { side = 2; vehicle = "FGWF_I_FCA_Companion_Ostap"; rank = "LIEUTENANT"; position[] = {0,0,0}; };
-                    class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Militia";         rank = "SERGEANT";   position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Militia";         rank = "CORPORAL";   position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Militia";         rank = "PRIVATE";    position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Militia";         rank = "PRIVATE";    position[] = {-10,-10,0}; };
-                    class Unit5 { side = 2; vehicle = "FGWF_I_FCA_Militia";         rank = "PRIVATE";    position[] = {15,-15,0}; };
-                    class Unit6 { side = 2; vehicle = "FGWF_I_FCA_Militia";         rank = "PRIVATE";    position[] = {-15,-15,0}; };
-                };
-
-                class GVAR(Group_InfantrySquad_Stepan) {
-                    name = CSTRING(Group_InfantrySquad_Stepan);
-                    side = 2;
-                    faction = QGVAR(FCA);
-                    icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
-
-                    class Unit0 { side = 2; vehicle = "FGWF_I_FCA_Companion_Stepan"; rank = "LIEUTENANT"; position[] = {0,0,0}; };
-                    class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";        rank = "SERGEANT";   position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";         rank = "CORPORAL";   position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";        rank = "PRIVATE";    position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";        rank = "PRIVATE";    position[] = {-10,-10,0}; };
-                    class Unit5 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";         rank = "PRIVATE";    position[] = {15,-15,0}; };
-                    class Unit6 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";         rank = "PRIVATE";    position[] = {-15,-15,0}; };
-                };
-
-                class GVAR(Group_ShockSquad_Bohdan) {
-                    name = CSTRING(Group_ShockSquad_Bohdan);
+                class GVAR(Group_MilitiaSquad_Bohdan) {
+                    name = CSTRING(Group_MilitiaSquad_Bohdan);
                     side = 2;
                     faction = QGVAR(FCA);
                     icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
 
                     class Unit0 { side = 2; vehicle = "FGWF_I_FCA_Companion_Bohdan"; rank = "LIEUTENANT"; position[] = {0,0,0}; };
-                    class Unit1 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop";       rank = "SERGEANT";   position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop";       rank = "CORPORAL";   position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop";       rank = "PRIVATE";    position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_ShockTroop";       rank = "PRIVATE";    position[] = {-10,-10,0}; };
+                    class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "SERGEANT";   position[] = {5,-5,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "CORPORAL";   position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "PRIVATE";    position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "PRIVATE";    position[] = {-10,-10,0}; };
+                    class Unit5 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "PRIVATE";    position[] = {15,-15,0}; };
+                    class Unit6 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "PRIVATE";    position[] = {-15,-15,0}; };
+                };
+
+                class GVAR(Group_InfantrySquad_Ostap) {
+                    name = CSTRING(Group_InfantrySquad_Ostap);
+                    side = 2;
+                    faction = QGVAR(FCA);
+                    icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
+
+                    class Unit0 { side = 2; vehicle = "FGWF_I_FCA_Companion_Ostap"; rank = "LIEUTENANT"; position[] = {0,0,0}; };
+                    class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";       rank = "SERGEANT";   position[] = {5,-5,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";        rank = "CORPORAL";   position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";       rank = "PRIVATE";    position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Grenadier";       rank = "PRIVATE";    position[] = {-10,-10,0}; };
+                    class Unit5 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";        rank = "PRIVATE";    position[] = {15,-15,0}; };
+                    class Unit6 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";        rank = "PRIVATE";    position[] = {-15,-15,0}; };
+                };
+
+                class GVAR(Group_Army_FieldPatrol_Stepan) {
+                    name = CSTRING(Group_Army_FieldPatrol_Stepan);
+                    side = 2;
+                    faction = QGVAR(FCA);
+                    icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
+
+                    class Unit0 { side = 2; vehicle = "FGWF_I_FCA_Companion_Stepan"; rank = "LIEUTENANT"; position[] = {0,0,0}; };
+                    class Unit1 { side = 2; vehicle = "FGWF_I_FCA_Rifleman";         rank = "SERGEANT";   position[] = {5,-5,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "CORPORAL";   position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "PRIVATE";    position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_FCA_Militia";          rank = "PRIVATE";    position[] = {-10,-10,0}; };
                 };
             };
         };
