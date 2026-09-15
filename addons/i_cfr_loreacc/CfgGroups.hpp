e1a0c4bf5dc73a4b4d484840c4733dc25c8e340c
WOJTEK885f <WOJTEK885@protonmail.com>
2026-09-15 22:24:37 +0200

    Add cloned militia units from Player faction for use in groups to match original game groups

--- addons/i_cfr_loreacc/CfgGroups.hpp
+++ addons/i_cfr_loreacc/CfgGroups.hpp
@@ -12,13 +12,13 @@ class CfgGroups {
                     faction = QGVAR(CFR);
                     icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
 
-                    class Unit0 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "SERGEANT";   position[] = {0,0,0}; };
-                    class Unit1 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "CORPORAL";   position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_CFR_DM";        rank = "PRIVATE";    position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";    position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";    position[] = {-10,-10,0}; };
-                    class Unit5 { side = 2; vehicle = "FGWF_I_CFR_DM";        rank = "PRIVATE";    position[] = {15,-15,0}; };
-                    class Unit6 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";    position[] = {-15,-15,0}; };
+                    class Unit0 { side = 2; vehicle = "FGWF_I_CFR_Fighter";                     rank = "SERGEANT";   position[] = {0,0,0}; };
+                    class Unit1 { side = 2; vehicle = "FGWF_I_CFR_Grenadier";                   rank = "CORPORAL";   position[] = {5,-5,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_CFR_DM";                          rank = "PRIVATE";    position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_CFR_MilitiaRifleman_SteelHelmet"; rank = "PRIVATE";    position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_CFR_MilitiaSMGFighter";           rank = "PRIVATE";    position[] = {-10,-10,0}; };
+                    class Unit5 { side = 2; vehicle = "FGWF_I_CFR_MilitiaSniper";               rank = "PRIVATE";    position[] = {15,-15,0}; };
+                    class Unit6 { side = 2; vehicle = "FGWF_I_CFR_MaleVillager";                rank = "PRIVATE";    position[] = {-15,-15,0}; };
                 };
 
                 class GVAR(Group_ChernivkanDefenceArmy) {
@@ -27,13 +27,13 @@ class CfgGroups {
                     faction = QGVAR(CFR);
                     icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
 
-                    class Unit0 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "SERGEANT";   position[] = {0,0,0}; };
-                    class Unit1 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "CORPORAL";   position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";    position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "PRIVATE";    position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_CFR_DM";        rank = "PRIVATE";    position[] = {-10,-10,0}; };
-                    class Unit5 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";    position[] = {15,-15,0}; };
-                    class Unit6 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "PRIVATE";    position[] = {-15,-15,0}; };
+                    class Unit0 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "SERGEANT"; position[] = {0,0,0}; };
+                    class Unit1 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "CORPORAL"; position[] = {5,-5,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";  position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "PRIVATE";  position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_CFR_DM";        rank = "PRIVATE";  position[] = {-10,-10,0}; };
+                    class Unit5 { side = 2; vehicle = "FGWF_I_CFR_Fighter";   rank = "PRIVATE";  position[] = {15,-15,0}; };
+                    class Unit6 { side = 2; vehicle = "FGWF_I_CFR_Grenadier"; rank = "PRIVATE";  position[] = {-15,-15,0}; };
                 };
             };
 
@@ -46,13 +46,13 @@ class CfgGroups {
                     faction = QGVAR(CFR);
                     icon = "\A3\ui_f\data\map\markers\nato\n_inf.paa";
 
-                    class Unit0 { side = 2; vehicle = "FGWF_I_CFR_Companion_Olga"; rank = "LIEUTENANT"; position[] = {0,0,0}; };
-                    class Unit1 { side = 2; vehicle = "FGWF_I_CFR_Grenadier";      rank = "CORPORAL";   position[] = {5,-5,0}; };
-                    class Unit2 { side = 2; vehicle = "FGWF_I_CFR_DM";             rank = "PRIVATE";    position[] = {-5,-5,0}; };
-                    class Unit3 { side = 2; vehicle = "FGWF_I_CFR_Fighter";        rank = "PRIVATE";    position[] = {10,-10,0}; };
-                    class Unit4 { side = 2; vehicle = "FGWF_I_CFR_Fighter";        rank = "PRIVATE";    position[] = {-10,-10,0}; };
-                    class Unit5 { side = 2; vehicle = "FGWF_I_CFR_DM";             rank = "PRIVATE";    position[] = {15,-15,0}; };
-                    class Unit6 { side = 2; vehicle = "FGWF_I_CFR_Fighter";        rank = "PRIVATE";    position[] = {-15,-15,0}; };
+                    class Unit0 { side = 2; vehicle = "FGWF_I_CFR_Companion_Olga";            rank = "LIEUTENANT"; position[] = {0,0,0}; };
+                    class Unit1 { side = 2; vehicle = "FGWF_I_CFR_Fighter";                   rank = "CORPORAL";   position[] = {5,-5,0}; };
+                    class Unit2 { side = 2; vehicle = "FGWF_I_CFR_Grenadier";                 rank = "PRIVATE";    position[] = {-5,-5,0}; };
+                    class Unit3 { side = 2; vehicle = "FGWF_I_CFR_DM";                        rank = "PRIVATE";    position[] = {10,-10,0}; };
+                    class Unit4 { side = 2; vehicle = "FGWF_I_CFR_MilitiaRifleman_OldHelmet"; rank = "PRIVATE";    position[] = {-10,-10,0}; };
+                    class Unit5 { side = 2; vehicle = "FGWF_I_CFR_MilitiaSMGFighter";         rank = "PRIVATE";    position[] = {15,-15,0}; };
+                    class Unit6 { side = 2; vehicle = "FGWF_I_CFR_MilitiaSniper";             rank = "PRIVATE";    position[] = {-15,-15,0}; };
                 };
             };
         };
--- addons/i_cfr_loreacc/config.cpp
+++ addons/i_cfr_loreacc/config.cpp
@@ -16,8 +16,11 @@ class CfgPatches {
             "CUP_Weapons_M3A1",          // M3A1 SMG (Coral)
             "rhsgref_c_weapons",         // Panzerfaust 60
             "rhs_main",                  // Flak Vest (Custom)
-            "CUP_Creatures_Military_USMC", // Interceptor vest, M1 helmet
+            "CUP_Creatures_Military_USMC", // Interceptor vest, M1 helmet, Protec (Bike Helmet), MCCUU
             "CUP_Creatures_Military_SLA",  // SLA helmet (Old Helmet)
+            "CUP_Creatures_Military_Russia", // K6-3 (Steel Helmet)
+            "CUP_Creatures_Military_PMC",  // PMC uniforms (Riflemen/SMG), CIRAS vest (Sniper)
+            "CUP_Creatures_People_Civil_Chernarus", // Worker_02 uniform, Police_Holster vest (Villager)
             "cfp_uniforms",              // CFP_GUER_M81, CFP_GUER_M81Tee
             "cfp_vests"                  // CFP_Tactical1_M81
         };
@@ -25,6 +28,13 @@ class CfgPatches {
             "FGWF_I_CFR_Fighter",
             "FGWF_I_CFR_Grenadier",
             "FGWF_I_CFR_DM",
+            "FGWF_I_CFR_MilitiaRifleman_Light",
+            "FGWF_I_CFR_MilitiaRifleman_OldHelmet",
+            "FGWF_I_CFR_MilitiaRifleman_SteelHelmet",
+            "FGWF_I_CFR_MilitiaRifleman_BikeHelmet",
+            "FGWF_I_CFR_MilitiaSMGFighter",
+            "FGWF_I_CFR_MilitiaSniper",
+            "FGWF_I_CFR_MaleVillager",
             "FGWF_I_CFR_Companion_Olga"
         };
         weapons[] = {};
