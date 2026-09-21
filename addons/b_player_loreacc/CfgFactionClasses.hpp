4ad746914e9f918be5bfce1fbd21ea1a06399cfb
WOJTEK885f <WOJTEK885@protonmail.com>
2026-09-22 00:27:23 +0200

    Add flag for Player Faction and update CfgFactionClasses

--- addons/b_player_loreacc/CfgFactionClasses.hpp
+++ addons/b_player_loreacc/CfgFactionClasses.hpp
@@ -4,5 +4,6 @@ class CfgFactionClasses {
         priority = 1;
         side = 1;
         icon = "\a3\Data_f\cfgFactionClasses_BLU_ca.paa";
+        flag = "data\flags\flag_PlayerFaction_co.paa";
     };
 };
